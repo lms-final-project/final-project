8fa636585c8cc895a4655e8325ce83abe456ed7a
Al_sakhen <ahmadalsakhen36@gmail.com>
2022-10-27 00:03:56 +0300

    Create Courses Table In The Dashboard

--- app/Http/Livewire/Dashboard/CoursesComponent.php
+++ app/Http/Livewire/Dashboard/CoursesComponent.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Dashboard;
 
+use App\Models\Category;
 use App\Models\Course;
 use App\Models\User;
 use Illuminate\Support\Facades\Log;
@@ -10,14 +11,36 @@ use Livewire\Component;
 class CoursesComponent extends Component
 {
     public $courses;
+    public $categories;
+    // for filter
+    public $title;
+    public $category_id;
 
     public function __construct()
     {
-        $this->courses = Course::with(['type' , 'category'])->get();
+        $this->categories = Category::all();
+    }
+
+    public function acceptCourse($id){
+    }
+    public function rejectCourse(){
 
     }
+
     public function render()
     {
+        // model must be a query not colection
+        $courses = Course::query()->with(['category' , 'type']);
+        if($this->title){
+            $courses = $courses->where('title', 'like' , '%'.$this->title.'%');
+        }
+
+        if($this->category_id){
+            $courses = $courses->where('category_id' , $this->category_id);
+        }
+
+        $this->courses = $courses->get();
+
         return view('livewire.dashboard.courses-component');
     }
 }
