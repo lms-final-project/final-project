297288971a71b5c432e19d4d16ff6bdaf6b6e82c
AnwarMelhem <anwarmelhem2000@gmail.com>
2022-11-09 11:35:26 +0200

    changes

--- app/Http/Controllers/Dashboard/CourseController.php
+++ app/Http/Controllers/Dashboard/CourseController.php
@@ -5,9 +5,12 @@ namespace App\Http\Controllers\Dashboard;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Course;
 class CourseController extends Controller
 {
     public function index(){
-        return view('dashboard.Courses.index');
+        $courses=Course::all();
+       // dd($courses);
+        return view('dashboard.Courses.index',compact('courses'));
     }
 }
