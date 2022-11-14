d8b2c5a810f072a8ac717864ad4e69a7d9c8bcad
Al_sakhen <ahmadalsakhen36@gmail.com>
2022-11-14 23:59:48 +0300

    Edit On CourseContentController

--- app/Http/Controllers/Instructor/CourseContentController.php
+++ app/Http/Controllers/Instructor/CourseContentController.php
@@ -37,19 +37,11 @@ class CourseContentController extends Controller
      */
     public function store(Request $request )
     {
-        $course_heading = CourseHeading::find($request->course_heading_id);
-
-        if(count($course_heading->contents) > 0){
-            $course_heading->contents()->delete();
-        }
-
-        foreach( $request->contents as $key => $value ){
-            CourseContent::Create([
-                'course_heading_id' => $request->course_heading_id,
-                'title'             => $value
-            ]);
-        }
-        return redirect()->back()->with('success' , 'contents added successfully');
+        CourseContent::Create([
+            'course_heading_id' => $request->course_heading_id,
+            'title'             => $request->content,
+        ]);
+        return redirect()->back()->with('success' , 'content added successfully');
     }
 
     /**
