39cce2ff5081e11637406590a1368756cee43af7
EllanaShadeed518 <ellanashadeed1312@gmail.com>
2022-12-25 19:38:18 +0300

    certificate

--- routes/student.php
+++ routes/student.php
@@ -2,19 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 use App\Http\Controllers\Frontend\Student\CoursesController;
 use App\Http\Controllers\Frontend\Student\FeedbackController;
 use App\Http\Controllers\Frontend\Student\DashboardController;
+use App\Http\Controllers\Frontend\Student\CertificateController;
 use App\Http\Controllers\Frontend\Student\StudentProfileController;
 
 Route::prefix('student')->middleware('checkStudent:student')->group(function(){
     Route::get('/panel' , [DashboardController::class , 'index'])->name('student.panel');
     Route::get('/panel/{student}' , [DashboardController::class , 'update'])->name('student.ToInstructor');
     Route::get('/courses' , [CoursesController::class , 'registered_courses'])->name('student.courses');
-    Route::get('/certificate' , [DashboardController::class , 'certificate'])->name('student.certificate');
-    Route::get('/certificate_index' , [DashboardController::class , 'index_certificate'])->name('student.certificate.index');
-    Route::post('/certificate_delete/{certificate}' , [DashboardController::class , 'delete_certificate'])->name('student.certificate.delete');
-    Route::post('/print_certificate/{certificate}' , [DashboardController::class , 'print_certificate'])->name('student.certificate.print');
+    Route::get('/certificate' , [CertificateController::class , 'certificate'])->name('student.certificate');
+    Route::get('/certificate_index' , [CertificateController::class , 'index_certificate'])->name('student.certificate.index');
+    Route::post('/certificate_delete/{certificate}' , [CertificateController::class , 'delete_certificate'])->name('student.certificate.delete');
+    Route::post('/print_certificate/{certificate}' , [CertificateController::class , 'print_certificate'])->name('student.certificate.print');
 
     Route::resource('/profile', StudentProfileController::class);
     Route::get('/password',[StudentProfileController::class , 'password'])->name('password');
